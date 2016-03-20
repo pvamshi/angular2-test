8193d248f88f2a3d3d94abc23576fa50c3755915
Vamshi krishna <pvamshi@gmail.com>
2016-03-20 09:37:13 -0700

    Adding service and injecting

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {TestComponent} from './test.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App demo</h1><test>',
+    directives: [TestComponent]
 })
 export class AppComponent { }
--- app/boot.js
+++ app/boot.js
@@ -11,8 +11,10 @@ System.register(['angular2/platform/browser', './app.component'], function(expor
                 app_component_1 = app_component_1_1;
             }],
         execute: function() {
+            // import {TestComponent} from './test.component'
             browser_1.bootstrap(app_component_1.AppComponent);
         }
     }
 });
+// bootstrap(TestComponent);
 //# sourceMappingURL=boot.js.map
